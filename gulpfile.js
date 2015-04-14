68abdb7972a5d943091573c8fcaf15500e6b08c5
Johnny Wu <KarasAya@gmail.com>
2015-04-14 15:22:28 +0800

    update fireball-x/dev#553, move fire-console to builtin

--- gulpfile.js
+++ gulpfile.js
@@ -27,17 +27,21 @@ var task_package = function ( name ) {
     var destBinPath = 'bin/' + name + '/';
 
     var task_copy = 'package-' + name + '-copy';
+    var task_copy_ext = 'package-' + name + '-copy-ext';
     var task_js = 'package-' + name + '-js';
     var task_styl = 'package-' + name + '-styl';
 
-    task_copy_deps.push(task_copy);
-    task_min_deps.push(task_copy, task_js, task_styl);
-    task_dev_deps.push(task_copy, task_js, task_styl);
+    task_copy_deps.push(task_copy, task_copy_ext);
+    task_min_deps.push(task_copy, task_copy_ext, task_js, task_styl);
+    task_dev_deps.push(task_copy, task_copy_ext, task_js, task_styl);
 
     var copy_files = [
         basePath + '**/*',
         '!' + basePath + '**/*.js',
         '!' + basePath + '**/*.styl',
+    ];
+
+    var ext_files = [
         basePath + '**/ext/**/*',
     ];
 
@@ -53,6 +57,7 @@ var task_package = function ( name ) {
 
     var watcher = {
         copy: { files: copy_files, tasks: [task_copy] },
+        ext: { files: ext_files, tasks: [task_copy_ext] },
         js: { files: js_files, tasks: [task_js] },
         styl: { files: styl_files, tasks: [task_styl]}
     };
@@ -61,6 +66,11 @@ var task_package = function ( name ) {
     // copy
     gulp.task(task_copy, function() {
         return gulp.src( watcher.copy.files, {base: basePath} )
+        .pipe(gulp.dest(destBinPath))
+        ;
+    });
+    gulp.task(task_copy_ext, function() {
+        return gulp.src( watcher.ext.files, {base: basePath} )
         .pipe(gulp.dest(destBinPath))
         ;
     });
@@ -115,6 +125,7 @@ gulp.task('watch', function() {
     for ( var i = 0; i < package_wartchers.length; ++i ) {
         var watcher = package_wartchers[i];
         gulp.watch( watcher.copy.files, watcher.copy.tasks ).on ( 'error', gutil.log );
+        gulp.watch( watcher.ext.files, watcher.ext.tasks ).on ( 'error', gutil.log );
         gulp.watch( watcher.js.files, watcher.js.tasks ).on ( 'error', gutil.log );
         gulp.watch( watcher.styl.files, watcher.styl.tasks ).on ( 'error', gutil.log );
     }
