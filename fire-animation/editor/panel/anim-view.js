ff641e6538c0546d0d33c5b31036337c75dbe971
kamisama <1457434343@qq.com>
2015-05-15 16:37:55 +0800

    add needle

--- fire-animation/editor/panel/anim-view.js
+++ fire-animation/editor/panel/anim-view.js
@@ -2,6 +2,7 @@ Polymer(EditorUI.mixin({
 
     publish: {
         'mode': 'dropsheet', // curveview, dropsheet
+        frame: 0
     },
 
     observe: {
@@ -25,6 +26,8 @@ Polymer(EditorUI.mixin({
 
         // timline
         this.$.timeline.init();
+
+        this.frameChanged();
 
         this.modeChanged();
     },
@@ -65,6 +68,7 @@ Polymer(EditorUI.mixin({
             this.$.curveview.scaleAction(event);
         }
         this.$.timeline.scaleAction(event);
+        this.moveNeedle(this.$.dropsheet.view.valueToPixelH(this.frame));
     },
 
     _onMouseDown: function ( event ) {
@@ -137,6 +141,10 @@ Polymer(EditorUI.mixin({
         this.$.timeline.repaint();
     },
 
+    frameChanged: function () {
+        this.moveNeedle(this.$.dropsheet.view.valueToPixelH(this.frame));
+    },
+
     modeChanged: function () {
         if ( this.mode === 'dropsheet' ) {
             this.$.curveview.style.display = 'none';
@@ -162,6 +170,74 @@ Polymer(EditorUI.mixin({
             this.$.curveview.resize();
             this.$.curveview.repaint();
         }
+    },
+
+    moveNeedle: function (offsetX) {
+        var nextFrames = this.$.dropsheet.view.pixelToValueH(offsetX);
+        var integerPixel = this.$.dropsheet.view.valueToPixelH(Math.round(nextFrames));
+        this.frame = Math.round(nextFrames);
+        this.$.needle.style.left = integerPixel - 0.5;
+        this.$.mask.style.width = integerPixel < 0 ? 0 : integerPixel;
+        return this.frame;
+    },
+
+    dragNeedleAction: function (event) {
+        EditorUI.addDragGhost("col-resize");
+        var forward = false;
+        var rollBack = false;
+        var step = 0;
+        var time;
+        time = setInterval(function () {
+            if (forward) {
+                this.$.timeline.pan( -step, 0 );
+                this.$.dropsheet.pan( -step, 0 );
+                this.$.curveview.pan( -step, 0 );
+                this.frame += 3;
+                this.repaint();
+            }
+        }.bind(this), 20 * this.$.dropsheet.xAxisScale);
+
+        var left = this.$.needle.getBoundingClientRect().left - this.getBoundingClientRect().left;
+        var dragWidth = this.getBoundingClientRect().width;
+        var mousemoveHandle =  function (event) {
+            var dx = event.clientX - this._lastClientX;
+            if ( (left + dx) <= 0) {
+                this.$.needle.style.left = 0;
+                this.$.mask.style.width = 0;
+                this.frame = 0;
+                return;
+            }
+
+            if ( (left + dx) >= dragWidth - 50) {
+                step = (this.$.dropsheet.view.valueToPixelH(1) - this.$.dropsheet.view.valueToPixelH(0)) * 3;
+                forward = true;
+                return;
+            }
+
+            this.moveNeedle(left + dx);
+            this.$.tip.style.display = 'block';
+            this.$.tip.style.left = left + dx + 20;
+            this.$.tip.style.top = event.clientY - 20;
+            this.$.tip.innerHTML = this.frame;
+        }.bind(this);
+
+        var mouseupHandle = function (event) {
+            document.removeEventListener('mousemove', mousemoveHandle);
+            document.removeEventListener('mouseup', mouseupHandle);
+            this.$.tip.style.display = 'none';
+            forward = false;
+            EditorUI.removeDragGhost();
+        }.bind(this);
+
+        this._lastClientX = event.clientX;
+
+        document.addEventListener ( 'mousemove', mousemoveHandle );
+        document.addEventListener ( 'mouseup', mouseupHandle );
+    },
+
+    _clickMoveNeedle: function (event) {
+        var offsetX = event.clientX - this.getBoundingClientRect().left;
+        this.moveNeedle(offsetX);
     },
 
 }, EditorUI.resizable));
