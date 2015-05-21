3486ae5a04fa86f0a6f696d4a7dc40460c1e3485
kamisama <1457434343@qq.com>
2015-05-21 15:25:56 +0800

    增加image的border拖拽调整

--- fire-inspector/editor/panel/image-preview-field.js
+++ fire-inspector/editor/panel/image-preview-field.js
@@ -7,6 +7,13 @@ Polymer({
     info: "Unkown",
     rawTexture: null,
 
+    created: function () {
+        this.lines = [];
+    },
+
+    domReady: function () {
+    },
+
     resize: function () {
         if ( !this.asset)
             return;
@@ -58,6 +65,7 @@ Polymer({
                               0, 0, this.$.canvas.width, this.$.canvas.height
                              );
             }
+            this.updateLine();
         }
     },
 
@@ -74,5 +82,135 @@ Polymer({
 
             return;
         }
+    },
+
+    updateLine: function () {
+        var left = (this.getBoundingClientRect().width - (this.$.canvas.getBoundingClientRect().width/this.asset.width * this.asset.width)) / 2;
+        var top = (this.getBoundingClientRect().height - (this.$.canvas.getBoundingClientRect().height/this.asset.height * this.asset.height)) / 2;
+
+        var drawWidth = this.$.canvas.getBoundingClientRect().width /this.asset.width * this.asset.width;
+        var drawHeight = this.$.canvas.getBoundingClientRect().height/this.asset.height * this.asset.height;
+
+        var xRatio = this.$.canvas.width / this.asset.width;
+        var yRatio = this.$.canvas.height / this.asset.height;
+
+        var lastTop = this.asset.borderTop;
+        var lastleft = this.asset.borderLeft;
+        var lastBottom = this.asset.borderBottom;
+        var lastRight = this.asset.borderRight;
+
+        this.$.dragleft.style.height = drawHeight;
+        this.$.dragleft.style.left = left + (lastleft * xRatio);
+        this.$.dragleft.style.top = top;
+
+        this.$.dragtop.style.top = top + (lastTop * yRatio);
+        this.$.dragtop.style.width = drawWidth;
+        this.$.dragtop.style.left = left;
+
+        this.$.dragbottom.style.right = left;
+        this.$.dragbottom.style.top = top + this.$.canvas.getBoundingClientRect().height - (lastBottom * yRatio) ;
+        this.$.dragbottom.style.width = drawWidth;
+
+        this.$.dragright.style.top = top;
+        this.$.dragright.style.left = left + this.$.canvas.getBoundingClientRect().width - (lastRight * xRatio);
+        this.$.dragright.style.height = drawHeight;
+    },
+
+    drawDragCircle: function (event) {
+        event.stopPropagation();
+        var direction = event.toElement.parentElement;
+        EditorUI.addDragGhost("normal");
+        var left = (this.getBoundingClientRect().width - (this.$.canvas.getBoundingClientRect().width/this.asset.width * this.asset.width)) / 2;
+        var top = (this.getBoundingClientRect().height - (this.$.canvas.getBoundingClientRect().height/this.asset.height * this.asset.height)) / 2;
+
+        var lastTop = this.asset.borderTop;
+        var lastleft = this.asset.borderLeft;
+        var lastBottom = this.asset.borderBottom;
+        var lastRight = this.asset.borderRight;
+
+        var eleTop = direction.getBoundingClientRect().top - this.$.canvas.getBoundingClientRect().top;
+        var eleLeft = direction.getBoundingClientRect().left - this.$.canvas.getBoundingClientRect().left;
+        var eleRight = direction.getBoundingClientRect().left - this.getBoundingClientRect().left;
+        var eleBottom = direction.getBoundingClientRect().top - this.getBoundingClientRect().top;
+
+        var xRatio = this.$.canvas.width / this.asset.width;
+        var yRatio = this.$.canvas.height / this.asset.height;
+
+        var mousemoveHandle = function (event) {
+            var dx = event.clientX - this._lastClientX;
+            var dy = event.clientY - this._lastClientY;
+            switch (direction.id) {
+                case 'dragtop':
+                    if (lastTop + (dy / yRatio) <= 0) {
+                        direction.style.top = top;
+                        this.asset.borderTop = 0;
+                        return;
+                    }
+                    else if (lastTop + (dy / yRatio) >= this.asset.height) {
+                        direction.style.top = top + this.$.canvas.getBoundingClientRect().height + (this.asset.height / yRatio) ;
+                        this.asset.borderTop = this.asset.height;
+                        return;
+                    }
+                    direction.style.top = eleTop + top + dy;
+                    this.asset.borderTop = lastTop + (dy / yRatio);
+                break;
+                case 'dragbottom':
+                    if (lastBottom - (dy / yRatio) <= 0) {
+                        direction.style.top = top + this.$.canvas.getBoundingClientRect().height;
+                        this.asset.borderBottom = 0;
+                        return;
+                    }
+                    else if (lastBottom - (dy / yRatio) >= this.asset.height) {
+                        direction.style.top = top + this.$.canvas.getBoundingClientRect().height - (this.asset.height / yRatio) ;
+                        this.asset.borderBottom = this.asset.height;
+                        return;
+                    }
+                    direction.style.top = eleBottom + dy;
+                    this.asset.borderBottom = lastBottom - (dy / yRatio);
+                break;
+
+                case 'dragleft':
+                    if(lastleft + (dx / xRatio) <= 0) {
+                        direction.style.left = left;
+                        this.asset.borderLeft = 0;
+                        return;
+                    }
+                    else if (lastleft + (dx / xRatio) >= this.asset.width) {
+                        direction.style.left = left + this.$.canvas.getBoundingClientRect().width + (this.asset.width / xRatio);
+                        this.asset.borderLeft = this.asset.width;
+                        return;
+                    }
+                    direction.style.left = eleLeft + dx + left;
+                    this.asset.borderLeft = lastleft + (dx / xRatio);
+                break;
+
+                case 'dragright':
+                    if (lastRight - (dx / xRatio) <= 0) {
+                        direction.style.left = left + this.$.canvas.getBoundingClientRect().width;
+                        this.asset.borderRight = 0;
+                        return;
+                    }
+                    else if (lastRight - (dx / xRatio) >= this.asset.width) {
+                        direction.style.left = left + this.$.canvas.getBoundingClientRect().width - (this.asset.width / xRatio);
+                        this.asset.borderRight = this.asset.width;
+                        return;
+                    }
+                    direction.style.left = eleRight + dx;
+                    this.asset.borderRight = lastRight - (dx / xRatio);
+                break;
+            }
+            this.asset.dirty = true;
+        }.bind(this);
+
+        var mouseupHandle = function (event) {
+            document.removeEventListener('mousemove', mousemoveHandle);
+            document.removeEventListener('mouseup', mouseupHandle);
+            EditorUI.removeDragGhost();
+        }.bind(this);
+
+        this._lastClientX = event.clientX;
+        this._lastClientY = event.clientY;
+        document.addEventListener ( 'mousemove', mousemoveHandle );
+        document.addEventListener ( 'mouseup', mouseupHandle );
     },
 });
